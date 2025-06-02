46cbe3b354c165d76df6aba44aa0931d984dfb72
ssh-expitrans <sasha.expitrans@gmail.com>
2025-06-02 14:18:42 -0600

    mobile styles

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ btnLeft.addEventListener('click', () => {
 btnRight.addEventListener('click', () => {
   track.scrollBy({ left: 200, behavior: 'smooth' });
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const navToggle = document.getElementById("navToggle");
+  const navMenuList = document.querySelector("#navMenu ul");
+
+  navToggle.addEventListener("click", () => {
+    navMenuList.classList.toggle("show");
+  });
+});
+
